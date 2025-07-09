1ddbd962440701d2d60c2d7e710e43fe19ec1374
Eyohen <henry.eyo2@gmail.com>
2025-07-09 15:06:27 +0100

    made the index in model to see

--- models/index.js
+++ models/index.js
@@ -53,12 +53,23 @@ const config = require('../config/database');
 const env = process.env.NODE_ENV || 'development';
 const dbConfig = config[env];
 
-const sequelize = new Sequelize(
-  dbConfig.database,
+let sequelize;
+
+if (dbConfig.use_env_variable){
+  sequelize = new Sequelize(process.env[dbConfig.use_env_variable], dbConfig);
+} else {
+    dbConfig.database,
   dbConfig.username,
   dbConfig.password,
   dbConfig
-);
+}
+
+// const sequelize = new Sequelize(
+//   dbConfig.database,
+//   dbConfig.username,
+//   dbConfig.password,
+//   dbConfig
+// );
 
 // Import models
 const User = require('./User')(sequelize);
